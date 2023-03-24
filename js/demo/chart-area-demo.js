5c64a85cc7c66889bbb1e3f4db5d08a76ba00613
MaximoMeza3150 <75499931+MaximoMeza3150@users.noreply.github.com>
2023-03-24 10:10:49 -0500

    Updates

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -7,9 +7,9 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+    labels: ["Marzo 1", "Marzo 2", "Marzo 3", "Marzo 4", "Marzo 5", "Marzo 6", "Marzo 7", "Marzo 8", "Marzo 9", "Marzo 10", "Marzo 11", "Marzo 12", "Marzo 13"],
     datasets: [{
-      label: "Sessions",
+      label: "Total Emisiones",
       lineTension: 0.3,
       backgroundColor: "rgba(2,117,216,0.2)",
       borderColor: "rgba(2,117,216,1)",
@@ -20,7 +20,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBackgroundColor: "rgba(2,117,216,1)",
       pointHitRadius: 50,
       pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+      data: [10, 10, 12, 14, 25, 29, 33, 13, 16, 19, 12, 11, 10],
     }],
   },
   options: {
@@ -39,7 +39,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 40000,
+          max: 50,
           maxTicksLimit: 5
         },
         gridLines: {
--- js/demo/chart-bar-demo.js
+++ js/demo/chart-bar-demo.js
@@ -7,12 +7,12 @@ var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"],
     datasets: [{
-      label: "Revenue",
+      label: "Total",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [20, 25, 45, 10, 15, 9],
     }],
   },
   options: {
@@ -31,7 +31,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 50,
           maxTicksLimit: 5
         },
         gridLines: {
